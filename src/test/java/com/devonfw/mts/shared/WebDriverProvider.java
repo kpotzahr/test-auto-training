3887a0f726842fd553735238e950b8db037b6c5a
Kathrin Potzahr <kathrin.potzahr@capgemini.com>
2025-12-14 13:36:44 +0100

    disable password leak popup

--- src/test/java/com/devonfw/mts/shared/WebDriverProvider.java
+++ src/test/java/com/devonfw/mts/shared/WebDriverProvider.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WebDriverProvider {
     public static WebDriver provideNewWebDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
         options.addArguments("--lang=en");
+        final Map<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        chromePrefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", chromePrefs);
         return new ChromeDriver(options);
     }
 }
